9595279bcd88b4b5c5854148bb734eaaedce3e56
fab.cyl@gmail.com <fab.cyl@gmail.com>
2017-09-09 17:39:05 -0500

    temp.py

---
+++ temp.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+X = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]
+Y = [4,4,5,5,6,6,6,7,8,8,8,9,10,10,10,10,11,12,12,12,13,13,13]
+plt.plot(X, Y, linewidth=3.0, c='red', marker='*')
+plt.xlim(0,22)
+plt.ylim(0,15)
+plt.title('Numero de primos y hermanos por anio de vida')
+plt.ylabel('Numero de primos y hermanos por anio')
+plt.xlabel('Numero de anios')
+plt.savefig('temp.png')
+plt.show()
+
